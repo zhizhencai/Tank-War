882a3a83fc9e79ad313eadb0ec176580587ea623
zhizhencai <caicleyton@gmail.com>
2020-09-24 17:32:41 -0400

    add comment

---
+++ src/tank1/TankFrame.java
@@ -0,0 +1,87 @@
+package tank1;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("tank war");
+        setVisible(true);
+
+        addKeyListener(new MyKeyListener());
+
+        addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+//        x+=
+    }
+
+    class MyKeyListener extends KeyAdapter {
+
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // zhizhen cai
+    }
+}
